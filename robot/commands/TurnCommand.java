55b634cd62a5dbc2d5234d9df8139dd24e3288fa
pkusalas <64712559+pkusalas@users.noreply.github.com>
2022-02-05 17:47:00 -0500

    Add files via upload

---
+++ robot/commands/TurnCommand.java
@@ -0,0 +1,23 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Robot;
+
+public class TurnCommand extends CommandBase {
+    double distance;
+    public TurnCommand(double inches) {
+        distance = inches;
+        
+    }
+
+    @Override
+    public void initialize() {
+        Robot.drivetrain.zeroSensors();
+        Robot.drivetrain.turnForInches(distance);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return Robot.drivetrain.isAtTarget();
+    }
+}
