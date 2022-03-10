8a3e02cff8d7a7fd1994eace13f93901b43866be
pkusalas <64712559+pkusalas@users.noreply.github.com>
2022-03-09 23:14:10 -0500

    Add files via upload

---
+++ robot/subsystem/Arm.java
@@ -0,0 +1,88 @@
+package frc.robot.subsystem;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class Arm extends SubsystemBase{
+    public CANSparkMax armMotor;
+    public RelativeEncoder armEncoder;
+
+    double power;
+    double powerFactor;
+    double maxEncoderValue = 19;
+
+    private boolean yWasPressed;
+    private boolean aWasPressed;
+
+    private static Arm instance = null;
+    
+    public static Arm getInstance() {
+        if (instance == null) {
+            instance = new Arm();
+        }
+        return instance;
+    }
+
+    public Arm() {
+        armMotor = new CANSparkMax(10, MotorType.kBrushless);
+        armEncoder = armMotor.getEncoder();
+        armMotor.setIdleMode(IdleMode.kBrake);
+    }
+
+    public void resetEncoder(){
+        armEncoder.setPosition(0);
+    }
+
+    public void stop() {
+        armMotor.set(0);
+    }
+
+    public void armPeriodic() {
+        powerFactor = 0.275;
+        power = -OI.getInstance().armController.getRawAxis(5) * powerFactor;
+       
+
+        if (OI.getInstance().armController.getRawButton(5)) {
+            power = -0.2;
+        } else if (armEncoder.getPosition() <= 4 && armEncoder.getPosition() > 0) {
+            if (power < 0) {
+                powerFactor = 0.025;
+            } 
+        } else if (armEncoder.getPosition() <= 0) {
+            if (power < 0) {
+                powerFactor = 0;
+            }
+        } else if (armEncoder.getPosition() >= maxEncoderValue) {
+            if (power > 0) {
+                power = 0;
+            }
+        }
+
+        armMotor.set(power);
+    }
+
+    public void updateMaxEncoderValue() {
+        if (OI.getInstance().armController.getRawButton(4)) {
+            if (yWasPressed == false) {
+                yWasPressed = true;
+                maxEncoderValue += 0.5;
+            }
+        } else {
+            yWasPressed = false;
+        }
+
+
+        if (OI.getInstance().armController.getRawButton(1)) {
+            if (aWasPressed == false) {
+                aWasPressed = true;
+                maxEncoderValue -= 0.5;
+            }
+        } else {
+            aWasPressed = false;
+        }
+    }
+}
---
+++ robot/subsystem/Intake.java
@@ -0,0 +1,48 @@
+package frc.robot.subsystem;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+
+public class Intake extends SubsystemBase{
+    public static TalonSRX intakeMotor;
+
+
+    private static Intake instance = null;
+    public static Intake getInstance() {
+        if (instance == null) {
+            instance = new Intake();
+        }
+        return instance;
+    }
+
+    public Intake() {
+        intakeMotor = new TalonSRX(5);
+    }
+
+
+    public void intakePeriodic() {
+        if (OI.getInstance().armController.getRawButton(5)) {
+            rollIn();
+        } else if (OI.getInstance().armController.getRawButton(6)) {
+            rollOut();
+        } else {
+            stop();
+        }
+    }
+
+
+    public void rollOut() {
+        intakeMotor.set(ControlMode.PercentOutput, 0.75);
+    }
+
+    public void rollIn() {
+        intakeMotor.set(ControlMode.PercentOutput, -1);
+    }
+
+    public void stop() {
+        intakeMotor.set(ControlMode.PercentOutput, 0);
+    }
+}
